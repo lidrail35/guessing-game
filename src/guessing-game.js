367798bbdb0c7ed8b24285c0b2f055f904fad6de
lidrail35 <lidrail35@mail.ru>
2023-02-20 22:11:27 +0300

    feat: add solve

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      let max;
+      let min;
+      let total;      
+    }
 
     setRange(min, max) {
-
+      this.min = min;  
+      this.max = max;
     }
 
     guess() {
-
+      this.total = Math.ceil((this.max + this.min)/2);
+      //console.log(this.min, this.max, this.total);
+      return this.total;
     }
 
     lower() {
-
+      this.max = this.total;
     }
 
     greater() {
-
+       this.min = this.total;
     }
 }
 
